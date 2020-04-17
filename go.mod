11b911e7b6ecec50ffa4ca8036a24dea980a75e9
Mahendra Bagul <mahendra@infracloud.com>
2020-04-17 20:04:30 +0530

    First Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/m
+
+go 1.14
+
+require github.com/mahendrabagul/FirstTestApp v0.0.0-20200417114820-4001a5d72d96
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mahendrabagul/FirstTestApp/greet"
+)
+
+func main() {
+	fmt.Println("Running the TestApp")
+	fmt.Println(greet.Hello("Rahul"))
+}
